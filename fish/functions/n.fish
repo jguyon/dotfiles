7e50f29394cc0337105ccc6be28186a5264f8ab8
Jérôme Guyon <jeromep.guyon@gmail.com>
2018-03-24 19:15:39 +0100

    Open vim sessions automatically

--- fish/functions/n.fish
+++ fish/functions/n.fish
@@ -1,3 +1,12 @@
 function n
-  nvim $argv
+  if test (count $argv) -gt 0
+    # if arguments are given, open nvim normally
+    nvim $argv
+  else if test -f Session.vim
+    # if a Session.vim file exists, open the session
+    nvim -S
+  else
+    # otherwise, create a new session
+    nvim -c Obsess
+  end
 end
