852700ae216c2244d3b1492587b040bba9616bfc
Jérôme Guyon <jeromep.guyon@gmail.com>
2018-04-27 04:20:40 +0200

    Fix the ssh alias, for realsies

--- snowblocks/fish/functions/s.fish
+++ snowblocks/fish/functions/s.fish
@@ -1,9 +1,15 @@
 function s
   if test (count $argv) -gt 1
-    # if more than zero arguments are given,
+    # if more than one argument is given,
     # call ssh with the first argument as host
     # and the rest as fish arguments
     ssh "$argv[1]" -t "bash -l -c 'fish $argv[2..-1]'"
+    and set -U s_last_host $argv[1]
+  else if test (count $argv) -eq 1
+    # if exactly one argument is given,
+    # call ssh with the argument as host
+    # and launch fish
+    ssh "$argv[1]" -t "bash -l -c fish"
     and set -U s_last_host $argv[1]
   else
     # if no arguments are given,
