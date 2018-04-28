3d72d0663670498e9d9792244c81d109d5df0b97
Jérôme Guyon <jeromep.guyon@gmail.com>
2018-04-28 13:41:58 +0200

    Tweak terminal colors

--- snowblocks/hyper/hyper.js
+++ snowblocks/hyper/hyper.js
@@ -1,14 +1,44 @@
+const colors = {
+  black: "#e7e5e2",
+  red: "#aa586e",
+  green: "#608f8e",
+  yellow: "#447487",
+  blue: "#56759a",
+  magenta: "#7c6a93",
+  cyan: "#a05b89",
+  white: "#697383",
+  colorCubes: "#dfddd7",
+  grayscale: "#929cad"
+};
+
 module.exports = {
   config: {
+    shell: "fish",
     fontSize: 13,
     fontFamily: "Iosevka, monospace",
     uiFontFamily: "Iosevka, monospace",
-    cursorShape: "BLOCK",
     padding: "0 0",
-    shell: "fish"
+    cursorShape: "BLOCK",
+    cursorColor: colors.white,
+    cursorAccentColor: colors.black,
+    backgroundColor: colors.white,
+    foregroundColor: colors.black,
+    selectionColor: colors.colorCubes,
+    borderColor: colors.grayscale,
+    colors: {
+      ...colors,
+      lightBlack: colors.black,
+      lightRed: colors.red,
+      lightGreen: colors.green,
+      lightYellow: colors.yellow,
+      lightBlue: colors.blue,
+      lightMagenta: colors.magenta,
+      lightCyan: colors.cyan,
+      lightWhite: colors.white
+    }
   },
 
-  plugins: ["nord-hyper", "hyperminimal"],
+  plugins: ["hyperminimal"],
   localPlugins: [],
 
   keymaps: {}
--- snowblocks/nvim/init.vim
+++ snowblocks/nvim/init.vim
@@ -233,14 +233,14 @@ hi! link ALEError NONE
 hi! link ALEWarning NONE
 hi! link ColorColumn CursorLine
 
-let g:terminal_color_0 = '#697383'
+let g:terminal_color_0 = '#e7e5e2'
 let g:terminal_color_1 = '#aa586e'
 let g:terminal_color_2 = '#608f8e'
 let g:terminal_color_3 = '#447487'
 let g:terminal_color_4 = '#56759a'
 let g:terminal_color_5 = '#7c6a93'
 let g:terminal_color_6 = '#a05b89'
-let g:terminal_color_7 = '#dfddd7'
+let g:terminal_color_7 = '#697383'
 let g:terminal_color_8 = g:terminal_color_0
 let g:terminal_color_9 = g:terminal_color_1
 let g:terminal_color_10 = g:terminal_color_2
