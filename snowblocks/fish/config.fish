286b2314667b619257b48f9d28d6ec898176dce8
Jérôme Guyon <jeromep.guyon@gmail.com>
2018-05-25 21:25:41 +0200

    Add binaries installed with yarn to PATH in fish

--- snowblocks/fish/config.fish
+++ snowblocks/fish/config.fish
@@ -6,5 +6,10 @@ if test -d $HOME/.local/bin
   set -gx PATH $HOME/.local/bin $PATH
 end
 
+# add binaries installed with yarn to PATH
+if type -q yarn
+  set -gx PATH (yarn global bin) $PATH
+end
+
 # configure fzf search command
 set -gx FZF_DEFAULT_COMMAND 'ag -l --nocolor -g ""'
