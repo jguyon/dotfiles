39c65cbc9fc2e1c46578d762ef5ecfd22f13543d
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-04-23 21:39:59 +0200

    Setup fullscreen action in alacritty
    No need for that hack with wmctrl anymore

--- fish/functions/f.fish
+++
@@ -1,9 +0,0 @@
-# toggle fullscreen
-function f
-  if type -q wmctrl
-    wmctrl -r :ACTIVE: -b toggle,fullscreen
-  else
-    echo "Please install wmctrl to toggle fullscreen"
-    false
-  end
-end
