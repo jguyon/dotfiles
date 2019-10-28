2b5a11406d74c592a36dc919f097f6f9e41f1598
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-10-28 12:02:43 +0100

    Simplify fish prompt

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,20 +1,2 @@
 function fish_mode_prompt
-  if [ "$fish_key_bindings" = "fish_vi_key_bindings" ]
-    set -l mode
-
-    switch $fish_bind_mode
-      case default
-        set mode (set_color magenta)n
-      case insert
-        set mode (set_color cyan)i
-      case replace_one
-        set mode (set_color red)r
-      case visual
-        set mode (set_color green)v
-    end
-
-    echo -n $mode ♦ (set_color normal)
-  else
-    fish_default_mode_prompt
-  end
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,53 +1,21 @@
-set __fish_git_prompt_color_branch blue
-
-set __fish_git_prompt_char_upstream_prefix ' '
-set __fish_git_prompt_char_stateseparator ' '
-
-set __fish_git_prompt_show_informative_status true
-set __fish_git_prompt_color_upstream_ahead yellow
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_color_upstream_behind yellow
-set __fish_git_prompt_char_upstream_behind '↓'
-
-set __fish_git_prompt_color_untrackedfiles red
-set __fish_git_prompt_char_untrackedfiles '*'
-
-set __fish_git_prompt_showdirtystate true
-set __fish_git_prompt_color_dirtystate red
-set __fish_git_prompt_char_dirtystate '±'
-
-set __fish_git_prompt_color_stagedstate yellow
-set __fish_git_prompt_char_stagedstate '⇈'
-
-set __fish_git_prompt_showuntrackedfiles true
-set __fish_git_prompt_color_cleanstate brwhite
-set __fish_git_prompt_char_cleanstate '✔'
-
 function fish_prompt
   set -l exit_code $status
-  set -l prompt
+  set -l color
 
-  # display username and hostname if logged in as root, in sudo or ssh session
-  if [ \( (id -u) -eq 0 -o $SUDO_USER \) -o $SSH_CONNECTION ]
-    set -l user (set_color yellow)$USER
-    set -l sep (set_color brwhite)@
-    set -l host (set_color cyan)(prompt_hostname)
-    set prompt $prompt $user$sep$host
+  switch $fish_bind_mode
+    case default
+      set color magenta
+    case replace_one
+      set color yellow
+    case visual
+      set color cyan
+    case '*'
+      if [ $exit_code != 0 ]
+        set color red
+      else
+        set color green
+    end
   end
 
-  # path
-  set prompt $prompt (set_color brwhite)(prompt_pwd)
-
-  # git
-  set -l branch_color (set_color $__fish_git_prompt_color_branch)
-  set prompt $prompt (__fish_git_prompt (set_color brblack)' %s')
-
-  # prompt symbol
-  if [ $exit_code != 0 ]
-    set prompt $prompt (set_color red)●
-  else
-    set prompt $prompt (set_color green)●
-  end
-
-  echo -n $prompt (set_color normal)
+  echo -n (set_color $color) '▲' (set_color normal)
 end
