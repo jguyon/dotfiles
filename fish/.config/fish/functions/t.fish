bebf3daa57dc401f0842991f482f90c9a54cfdbf
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-06-21 22:49:48 +0200

    Remove useless tmux alias

--- fish/.config/fish/functions/t.fish
+++
@@ -1,9 +0,0 @@
-function t
-  if test (count $argv) -gt 0
-    # if arguments are given, open tmux normally
-    tmux $argv
-  else
-    # otherwise, attach to last session or create a new one
-    tmux attach ; or tmux new
-  end
-end
